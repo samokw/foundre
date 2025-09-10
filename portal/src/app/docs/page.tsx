31c79d6159c68681984327dd44d8a9ae77f275e1
Samuel Okwusiuno <okwusiunosamuel@gmail.com>
2025-09-10 11:55:50 -0400

    created basic landing page using nextjs

---
+++ portal/src/app/docs/page.tsx
@@ -0,0 +1,10 @@
+export default function HomePage() {
+  return (
+    <section className="flex flex-col items-center justify-center text-center py-20">
+      <h1 className="text-4xl font-bold mb-4">foundre documentation</h1>
+      <p className="text-lg text-gray-600 dark:text-gray-300 max-w-xl">
+        deployment made easy.
+      </p>
+    </section>
+  );
+}
